b707850fa4062057730709d31db459aaaca664e0
Rizqi Raditya <radityaparamita@gmail.com>
2025-12-30 19:57:58 +0700

    fix: memperbarui bulk action dan menambahkan jumlah data di setiap tab komplek

--- app/Filament/Resources/BlockResource/Pages/ListBlocks.php
+++ app/Filament/Resources/BlockResource/Pages/ListBlocks.php
@@ -3,11 +3,11 @@
 namespace App\Filament\Resources\BlockResource\Pages;
 
 use App\Filament\Resources\BlockResource;
+use App\Models\Block;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Components\Tab;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ListBlocks extends ListRecords
 {
@@ -22,18 +22,32 @@ class ListBlocks extends ListRecords
 
     public function getTabs(): array
     {
-        if (!auth()->user()?->hasRole('super_admin')) {
+        if (! auth()->user()?->hasRole('super_admin')) {
             return [];
         }
 
         return [
             'aktif' => Tab::make('Data Aktif')
-                ->modifyQueryUsing(fn(Builder $query) => $query),
-            'terhapus' => Tab::make('Data Terhapus')
-                ->modifyQueryUsing(
-                    fn(Builder $query) =>
-                    $query->withoutGlobalScopes([SoftDeletingScope::class])->onlyTrashed()
+                ->modifyQueryUsing(fn (Builder $query) => $query->withoutTrashed())
+                ->badge(fn () => Block::query()
+                    ->whereHas('dorm')
+                    ->withoutTrashed()
+                    ->count()
                 ),
+
+            'terhapus' => Tab::make('Data Terhapus')
+                ->modifyQueryUsing(fn (Builder $query) => $query->onlyTrashed())
+                ->badge(fn () => Block::query()
+                    ->whereHas('dorm')
+                    ->onlyTrashed()
+                    ->count()
+                )
+                ->badgeColor('danger'),
         ];
+    }
+
+    public function updatedActiveTab(): void
+    {
+        $this->deselectAllTableRecords();
     }
 }
