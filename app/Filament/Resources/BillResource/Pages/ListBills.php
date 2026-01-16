c33fe84106861ced11c5df02e32d81316b46c5bd
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2026-01-16 10:51:41 +0700

    feat: membuat statistik untuk halaman tagihan

--- app/Filament/Resources/BillResource/Pages/ListBills.php
+++ app/Filament/Resources/BillResource/Pages/ListBills.php
@@ -3,18 +3,48 @@
 namespace App\Filament\Resources\BillResource\Pages;
 
 use App\Filament\Resources\BillResource;
+use App\Models\Bill;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class ListBills extends ListRecords
 {
     protected static string $resource = BillResource::class;
+
+    public function getTabs(): array
+    {
+        if (! auth()->user()?->hasRole('super_admin')) {
+            return [];
+        }
+
+        return [
+            'aktif' => Tab::make('Data Aktif')
+                ->badge(fn() => Bill::query()->withoutTrashed()->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->withoutTrashed()),
+
+            'terhapus' => Tab::make('Data Terhapus')
+                ->badge(fn() => Bill::query()->onlyTrashed()->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->onlyTrashed())
+                ->badgeColor('danger'),
+        ];
+    }
+
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make()
                 ->label('Buat Tagihan')
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            BillResource\Widgets\BillStatsOverview::class
         ];
     }
 }
