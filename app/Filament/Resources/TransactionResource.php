c138905f48f92fdbd7e4f236033f428bec5d7ef4
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2026-01-31 20:13:57 +0700

    refactor: mengubah logika sistem pembayaran

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -18,7 +18,7 @@ class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-banknotes';
+    protected static ?string $navigationGroup = 'Keuangan';
     protected static ?string $navigationLabel = 'Arus Kas';
     protected static ?string $modelLabel = 'Transaksi';
     protected static ?string $pluralModelLabel = 'Arus Kas';
